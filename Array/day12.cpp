df65a7827a2ca68370b0b227ec34aee379bac567
RA4m15 <r9807537@gmail.com>
2026-05-20 22:36:44 +0530

    prefix common array

---
+++ Array/day12.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        vector<int> arr(n+1,0);
+        vector<int> ans(n);
+        int i = 0;
+        int count = 0;
+        for(int i = 0;i<n;i++){
+            arr[A[i]]++;
+            if(arr[A[i]]==2){
+                count++;
+            }
+            arr[B[i]]++;
+            if(arr[B[i]]==2){
+                count++;
+            }
+            ans[i] = count;
+        }
+        return ans;
+    }
+};
