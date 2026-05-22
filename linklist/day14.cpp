eebeac211edf5807775918eb6aa7ad7d7de10a9e
RA4m15 <r9807537@gmail.com>
2026-05-22 20:52:11 +0530

    Linklist implementation

---
+++ linklist/day14.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+struct Node{
+    int data;
+    Node *next;
+
+    Node(int data1,Node* next1){
+        data = data1;
+        next = next1;
+    }
+    Node(int data1){
+        data = data1;
+        next = NULL;
+    }
+};
+Node* convertarrtoLL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i =1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+int main(){
+    vector<int> arr = {1,4,5,6};
+    Node* head = convertarrtoLL(arr);
+    cout<<head->data;
+}
