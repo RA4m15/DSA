62384e2399bfd87de77d54e22039b4d07b022d6b
RA4m15 <r9807537@gmail.com>
2026-06-04 21:57:38 +0530

    check duplicater in the array

--- strings/sum.cpp
+++ strings/sum.cpp
@@ -1,8 +1,11 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int a = 10;
-    int b = 20;
-    cout << a + b << endl;
-    return 0;
-}
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i = 0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1])
+            return true;
+        }
+        return false;
+    }
+};
