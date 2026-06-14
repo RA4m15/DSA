16b7d33a39cf7ef5011d03893743babf19abf554
RA4m15 <r9807537@gmail.com>
2026-06-14 21:35:30 +0530

    max 69 number

---
+++ strings/max69.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    int maximum69Number (int num) {
+        int newnum = 0;
+        string s = to_string(num);
+        for(int i=0; i<s.length(); i++){
+            if(s[i] == '6'){
+                s[i] = '9';
+                break;
+            }
+        }
+        return stoi(s);
+    }
+};
+int main(){
+    Solution s1;
+    int num = 9669;
+    cout<<s1.maximum69Number(num)<<" ";
+    return 0;
+}
